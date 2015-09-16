f8f314a3dd085046dbefe5bd648dbe480ce62252
Stefan Zerkalica <zerkalica@gmail.com>
2015-09-16 14:55:27 +0300

    added purerender

---
+++ src/purerender.js
@@ -0,0 +1,9 @@
+import deepEqual from 'deeper'
+
+function shouldComponentUpdate(nextProps, nextState) {
+    return deepEqual(this.props, nextProps)
+}
+
+export default function purerender(Component) {
+    Component.prototype.shouldComponentUpdate = shouldComponentUpdate
+}
