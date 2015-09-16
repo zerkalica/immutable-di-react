9e711251ad73ba8d62cafb54fbccbf71384bf454
Stefan Zerkalica <zerkalica@gmail.com>
2015-09-16 17:50:52 +0300

    root interface changed

--- src/root.js
+++ src/root.js
@@ -3,11 +3,12 @@ import Container from 'immutable-di/container'
 import getFunctionName from 'immutable-di/utils/getFunctionName'
 import NativeCursor from 'immutable-di/cursors/native'
 
+function pass(p) {
+    return p
+}
+
 export class RootComponent extends Component {
-    static propTypes = {
-        container: p.instanceOf(Container),
-        state: p.object
-    }
+    static _getState = pass
 
     static childContextTypes = {
         container: p.instanceOf(Container).isRequired
@@ -15,18 +16,31 @@ export class RootComponent extends Component {
 
     getChildContext() {
         return {
-            container: this.props.container || new Container(new NativeCursor(this.props.state))
+            container: this.__container
         }
+    }
+
+    constructor(props, context) {
+        super(props, context)
+        this.__container = props.container
+            ? props.container
+            : new Container(new NativeCursor(this.constructor._getState(props)))
     }
 }
 
-export default function root(BaseComponent) {
-    const dn = BaseComponent.displayName || getFunctionName(BaseComponent)
-    return class RootComponentWrapper extends RootComponent {
-        static displayName = dn + '_root'
 
-        render() {
-            return createElement(BaseComponent, this.props)
+export default function root(getDefaultState) {
+    const getState = getDefaultState || pass
+
+    return function rootWrapper(BaseComponent) {
+        const dn = BaseComponent.displayName || getFunctionName(BaseComponent)
+        return class RootComponentWrapper extends RootComponent {
+            static displayName = dn + '_root'
+            static _getState = getState
+
+            render() {
+                return createElement(BaseComponent, this.props)
+            }
         }
     }
 }
