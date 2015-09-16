5b0c3abc244d23753a6d5c23231be1ee7d753dcd
Stefan Zerkalica <zerkalica@gmail.com>
2015-09-16 15:01:00 +0300

    fix

--- src/purerender.js
+++ src/purerender.js
@@ -1,9 +1,11 @@
 import deepEqual from 'deeper'
 
 function shouldComponentUpdate(nextProps, nextState) {
-    return deepEqual(this.props, nextProps)
+    return !deepEqual(this.props, nextProps)
 }
 
 export default function purerender(Component) {
     Component.prototype.shouldComponentUpdate = shouldComponentUpdate
+
+    return Component
 }
